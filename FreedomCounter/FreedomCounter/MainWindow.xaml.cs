bcad3a41e55632bc3c4c264b3f760a047fcc19c9
Lars Lindqvist <lars.lindqvist@combitech.se>
2019-11-14 13:19:03 +0100

    Added more window features

--- FreedomCounter/FreedomCounter/MainWindow.xaml.cs
+++ FreedomCounter/FreedomCounter/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using MouseButton = System.Windows.Input.MouseButton;
 
 namespace FreedomCounter
 {
@@ -23,20 +24,62 @@ namespace FreedomCounter
     {
         DispatcherTimer _timer;
         TimeSpan _time;
+        private int count = 0;
         public MainWindow()
         {
             InitializeComponent();
+            if (SettingsButton.Background.IsFrozen)
+            {
+                SettingsButton.Background = SettingsButton.Background.Clone();
+            }
+            SettingsButton.Background.Opacity = 0;
 
+            if (ExitButton.Background.IsFrozen)
+            {
+                ExitButton.Background = ExitButton.Background.Clone();
+            }
+            ExitButton.Background.Opacity = 0;
+
+            if (ContentGrid.Background.IsFrozen)
+            {
+                ContentGrid.Background = ContentGrid.Background.Clone();
+            }
+            ContentGrid.Background.Opacity = 0.01;
             _time = TimeSpan.FromSeconds(120);
-
             _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
             {
-                tbTime.Text = _time.ToString("c");
+                timeLeft.Content = _time.ToString("c");
+                freeTime.Content = _time.ToString("c");
+                overtime.Content = _time.ToString("c");
                 if (_time == TimeSpan.Zero) _timer.Stop();
                 _time = _time.Add(TimeSpan.FromSeconds(-1));
             }, Application.Current.Dispatcher);
-
             _timer.Start();
+
+            toolBar.Visibility = Visibility.Collapsed;
+            ContentWindow.MouseMove += Window_MouseMove;
+            ContentWindow.MouseLeave += Window_MouseInOut;
+            ContentWindow.MouseEnter += Window_MouseInOut;
+
+        }
+
+        private void Window_MouseInOut(object sender, MouseEventArgs e)
+        {
+            if (ContentWindow.IsMouseOver)
+            {
+                toolBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                toolBar.Visibility = Visibility.Collapsed;
+            }
+        }
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
